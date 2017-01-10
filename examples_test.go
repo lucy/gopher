5a0afb6944ed2274ea189073c5c7c3e033dc78ca
lucy <lucy@luz.lu>
2017-01-10 10:41:18 +0100

    prettify example

--- examples_test.go
+++ examples_test.go
@@ -8,8 +8,11 @@ import (
 )
 
 func ExampleFileServer() {
-	srv := &gopher.Server{Addr: "127.0.0.1:7070",
-		ExtHost: "127.0.0.1", ExtPort: "7070",
-		Handler: gopher.FileServer(http.Dir("/usr/share/doc"))}
+	srv := &gopher.Server{
+		Addr:    "127.0.0.1:7070",
+		ExtHost: "127.0.0.1",
+		ExtPort: "7070",
+		Handler: gopher.FileServer(http.Dir("/usr/share/doc")),
+	}
 	log.Fatal(srv.ListenAndServe())
 }
